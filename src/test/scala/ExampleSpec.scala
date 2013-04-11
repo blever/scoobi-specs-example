b0379e696770f67501a4f876cfc51364c40de08c
Ben Lever <ben.lever@nicta.com.au>
2013-04-11 13:54:48 +1000

    Fix bug in expected reulst in example spec.

--- src/test/scala/ExampleSpec.scala
+++ src/test/scala/ExampleSpec.scala
@@ -4,6 +4,6 @@ import com.nicta.scoobi.testing.mutable.HadoopSpecification
 
 class ExampleSpec extends HadoopSpecification {
   "Can add 1 to DList elements" >> { implicit sc: ScoobiConfiguration =>
-    DList(1, 2, 3).map(_ + 1).run must_== Seq(1, 2, 3)
+    DList(1, 2, 3).map(_ + 1).run must_== Seq(2, 3, 4)
   }
 }
